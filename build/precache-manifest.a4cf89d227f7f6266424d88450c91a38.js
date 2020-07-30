6c7715d38edf099ed82ecd71ad75114fdeb3f8f6
andybi7676 <59821107+andybi7676@users.noreply.github.com>
2020-07-30 15:25:44 +0800

    20200730

---
+++ build/precache-manifest.a4cf89d227f7f6266424d88450c91a38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a20a36a4f5c4a840259bf410609add",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f9d5281e0dbb4db0cc",
+    "url": "/static/css/main.cc096785.chunk.css"
+  },
+  {
+    "revision": "c88c15aca2b490e55eed",
+    "url": "/static/js/2.d10d2446.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d10d2446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f9d5281e0dbb4db0cc",
+    "url": "/static/js/main.63650ba4.chunk.js"
+  },
+  {
+    "revision": "93b51166f3b499d40425",
+    "url": "/static/js/runtime-main.fbf42b67.js"
+  }
+]);
